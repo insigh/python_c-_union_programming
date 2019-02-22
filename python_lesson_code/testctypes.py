43d57441eace74ff8af11d0c5b2349697ea447f1
insigh <zhangchaojie@ruc.edu.cn>
2019-02-22 15:37:20 +0800

    rm sub work directory

--- python_lesson_code/testctypes.py
+++
@@ -1,5 +0,0 @@
-print("Test Ctypes")
-from ctypes import *
-# 导入库 window中dll后缀名不用加
-lib = CDLL("testctypes")
-lib.TestCtypes()
--- testctypes/testctypes.py
+++
@@ -1,5 +0,0 @@
-print("Test Ctypes!")
-from ctypes import *
-#倒入库 windows中dll后缀名不用加，Linux需要.so
-lib = CDLL("testctypes")
-lib.TestCtypes()
