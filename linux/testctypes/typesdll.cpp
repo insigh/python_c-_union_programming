54042001d18dd49203a28169363bafe6ba6ac998
insigh <zhanghchaojie@ruc.edu.cn>
2019-02-22 16:10:50 +0800

    linux first experience

--- linux/testctypes/typesdll.cpp
+++ linux/testctypes/typesdll.cpp
@@ -1,8 +1,14 @@
 #include<stdio.h>
 // c++ 中编译c格式的函数，如果用C语言编译就不需要（文件后缀名.c）
 // __declspec(dllexport) 表示函数导出到库中
+// 靠靠縒indows WIN32 _WIN32(靠win32縲in64)
+#ifdef _WIN32
+    #define XLIB __declspec(dllexport)
+#else 
+    #define XLIB
+#endif
 
-extern "C" __declspec(dllexport) void TestCtypes()
+extern "C" XLIB void TestCtypes()
 {
 	printf("In C TestCtypes!\n");
 }
